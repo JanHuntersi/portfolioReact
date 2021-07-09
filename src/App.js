c40277b814b23f1479374d0810e634e09c2d0109
Jan Sernec <jan.sernec@gmail.com>
2021-07-09 15:45:19 +0200

    spinn

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="App-logo">
           Jan Sernec
         </p>
       </header>
